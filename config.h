5f71ae039bd579481eadb834d1dc98642a8c96d6
John Magdy Lotfy Kamel <19735243+Zorono@users.noreply.github.com>
2024-05-29 23:45:20 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,3 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
 #define DEBUG_PROC true
 
 #define WEBsrv_PORT 4500
@@ -7,8 +10,14 @@
 #define SERIAL_TIMEOUT (10000UL)  // Maximum time to wait for serial activity to start
 
 #define DefaultWiFi "The Box @ Beast"
-#define WiFiChannel 11
+#define WiFiChannel 10
 #define MaxWiFiConnections 7
 #define WiFiHidden true
 
 #define BUZZER_PIN D3
+#define BUZZER_ENABLED false
+
+
+#define CONFIG_FILE "/config.json"
+
+#endif
