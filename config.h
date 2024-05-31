0779de111aeae1f71a9ee585c29d6951f1faef79
John Magdy Lotfy Kamel <19735243+Zorono@users.noreply.github.com>
2024-05-31 13:32:34 +0300

    Update config.h

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 #define CONFIG_H
 
 #define DEBUG_PROC true
+#define STARTUP_DELAY 1000
 
 #define WEBsrv_PORT 4500
 
@@ -13,8 +14,13 @@
 #define WiFiChannel 10
 #define MaxWiFiConnections 7
 #define WiFiHidden true
+#define WIFI_AUTOCONNECT true
+#define WIFI_AUTORECONNECT true
+#define WIFI_RECONNECT_TIMER 500
+#define WIFI_STA_HOSTNAME "Beast's AP"
+#define STATIC_DHCP_AP true
 
-#define BUZZER_PIN D3
+#define BUZZER_PIN 13 //GPIO 13 = D7
 #define BUZZER_ENABLED false
 
 
